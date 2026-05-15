715f8c709002a197ccb1334e692a57f4afcfedc4
evman89 <evman8989@yahoo.com>
2026-05-15 16:55:47 -0400

    Part 1 exercise 3 alternative

--- part1/course_info/src/App.jsx
+++ part1/course_info/src/App.jsx
@@ -5,12 +5,19 @@ const Header = (props) => {
   )
 }
 
-const Total = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+const Content = (props) => {
   return(
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <div>
+      <p>{props.part} {props.exercises}</p>
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+    <p>Number of exercises {props.totalexercises}</p>
+    </div>
   )
 }
 const App = () => {
@@ -27,13 +34,15 @@ const App = () => {
     name: 'State of a component',
     exercises: 14
   }
+  const totalexercises = part1.exercises + part2.exercises + part3.exercises
+
   return (
     <div>
       <Header course={course} />
-      <p/>{part1.name} {part1.exercises}<p/>
-      <p/>{part2.name} {part2.exercises}<p/>
-      <p/>{part3.name} {part3.exercises}<p/>
-      <Total />
+      <Content part={part1.name} exercises= {part1.exercises} />
+      <Content part={part1.name} exercises= {part1.exercises} />
+      <Content part={part1.name} exercises= {part1.exercises} />
+      <Total totalexercises={totalexercises}/>
     </div>
   )
 }
