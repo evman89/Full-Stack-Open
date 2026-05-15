dfd7aacd489403cca98f2a0af4d79af5a836677f
evman89 <evman8989@yahoo.com>
2026-05-15 09:54:27 -0400

    Part 1 exercise 1 Refactored App.jsx

--- part1/course_info/src/App.jsx
+++ part1/course_info/src/App.jsx
@@ -1,15 +1,21 @@
-const App = () => {
+const Header = () => {
   const course = 'Half Stack application development'
+  return(
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Content = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
-  return (
+  return(
     <div>
-      <h1>{course}</h1>
       <p>
         {part1} {exercises1}
       </p>
@@ -19,7 +25,24 @@ const App = () => {
       <p>
         {part3} {exercises3}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const Total = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return(
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Total />
     </div>
   )
 }
