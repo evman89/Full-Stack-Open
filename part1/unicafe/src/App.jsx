2c02b2e2414efb1b5a0eb7f06239486037061e43
evman89 <evman8989@yahoo.com>
2026-05-19 17:16:50 -0400

    Part 1 exercise 11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,7 +7,10 @@ const Button = (props) => {
 }
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td> {props.text} </td>
+      <td> {props.value} </td>
+    </tr>
   )
 }
 const Statistics = (props) => {
