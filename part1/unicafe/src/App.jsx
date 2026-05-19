992dbd7412ce4792af2d8d3cc6387e6a73fbc494
evman89 <evman8989@yahoo.com>
2026-05-19 16:04:05 -0400

    Part 1 exercise 7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -23,8 +23,11 @@ const App = () => {
       <button onClick = {handleBadClick}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
-      <p>neurtal {neutral}</p>
+      <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good + neutral*0 + bad*(-1))/(good + neutral + bad)}</p>
+      <p>positive {(good/(good + neutral + bad))*100} %</p>
     </div>
   )
 }
