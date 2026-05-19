7db6d28834d5252e73bb37e35749cba8f7a70fca
evman89 <evman8989@yahoo.com>
2026-05-19 16:23:03 -0400

    Part 1 exercise 9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
+  if (props.good + props.neutral + props.bad === 0) {
+    return (
+      
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  
   return(
     <div>
     <h1>statistics</h1>
